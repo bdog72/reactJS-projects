d1f3065d583a246365f5f12aece764b875bcabd5
Brian Bycynski <high5bri@hotmail.com>
2021-02-09 12:44:24 -0500

    refactoring project 1

--- src/projects-1-2-3/1-a/src/App.js
+++ src/projects-1-2-3/1-a/src/App.js
@@ -1,13 +1,16 @@
 //
 //
-import React from 'react';
 
-const App = () => {
+import React from 'react';
+import './app.css';
+// import './app.scss';
+
+function App() {
   return (
-    <div>
-      <h1>A</h1>
+    <div className='app__1-main-container'>
+      <h1>Birthday Reminder</h1>
     </div>
   );
-};
+}
 
 export default App;
