0209f56fb50c9e474d4c82f6bf69799332fed49b
Brian Bycynski <high5bri@hotmail.com>
2021-02-18 03:05:18 -0500

    finished lorem ipsum generator

--- src/projects-7-8-9/8-h/src/App.js
+++ src/projects-7-8-9/8-h/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 //
 //
 
@@ -8,9 +7,47 @@ import data from './data';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    if (count <= 0) {
+      amount = 1;
+    }
+
+    if (count > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
   return (
     <div className='app__8-main-container'>
-      <h2>lorem ipsum project setup</h2>
+      <section className='section-center'>
+        <h3>Tired of boring lorem Ipsum?</h3>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='amount'>paragraphs:</label>
+          <input
+            type='number'
+            name='amount'
+            id='amount'
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type='submit' className='btn'>
+            generate
+          </button>
+        </form>
+        <article className='lorem-text'>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
     </div>
   );
 }
